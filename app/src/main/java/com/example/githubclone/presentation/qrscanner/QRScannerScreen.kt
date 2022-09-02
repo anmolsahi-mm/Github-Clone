d0cd2d0bd33d087c1440d116e85d04772de42863
anmolsinghsahi <anmol.sahi@mutualmobile.com>
2022-09-02 11:43:48 +0530

    added torch and gallery icons, made a surface box to scan the code

--- app/src/main/java/com/example/githubclone/presentation/qrscanner/QRScannerScreen.kt
+++ app/src/main/java/com/example/githubclone/presentation/qrscanner/QRScannerScreen.kt
@@ -5,18 +5,28 @@ import android.content.pm.PackageManager
 import android.util.Size
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.Camera
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.CameraSelector.LENS_FACING_BACK
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
+import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -24,24 +34,32 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import com.example.githubclone.R
 import com.google.mlkit.vision.barcode.BarcodeScanner
 import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 import timber.log.Timber
 
 @Composable
 fun QRScannerScreen() {
-    val code by remember {
-        mutableStateOf("")
+    var code by remember {
+        mutableStateOf("Scanned Text Here")
     }
+    var isTorchEnable by remember { mutableStateOf(false) }
+    var camera: Camera? = null
 
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
@@ -69,43 +87,114 @@ fun QRScannerScreen() {
     }
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
     ) {
         if (hasCameraPermission) {
-            AndroidView(
-                factory = { context ->
-                    val previewView = PreviewView(context)
-                    val preview = Preview.Builder().build()
-                    val selector = CameraSelector.Builder()
-                        .requireLensFacing(LENS_FACING_BACK)
-                        .build()
-                    preview.setSurfaceProvider(previewView.surfaceProvider)
-                    val imageAnalysis = ImageAnalysis.Builder()
-                        .setTargetResolution(Size(previewView.width, previewView.height))
-                        .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
-                        .build()
-                    val barcodeScanner = BarcodeScanning.getClient()
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(10f)
+            ) {
+                AndroidView(
+                    factory = { context ->
+                        val previewView = PreviewView(context)
+                        val preview = Preview.Builder().build()
+                        val selector = CameraSelector.Builder()
+                            .requireLensFacing(LENS_FACING_BACK)
+                            .build()
+                        preview.setSurfaceProvider(previewView.surfaceProvider)
+                        val imageAnalysis = ImageAnalysis.Builder()
+                            .setTargetResolution(Size(previewView.width, previewView.height))
+                            .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
+                            .build()
+                        val barcodeScanner = BarcodeScanning.getClient()
+                        val imageCapture = ImageCapture.Builder()
+                            .setFlashMode(ImageCapture.FLASH_MODE_ON)
+                            .build()
 
-                    imageAnalysis.setAnalyzer(
-                        ContextCompat.getMainExecutor(context)
-                    ) { imageProxy ->
-                        processImageProxy(barcodeScanner, imageProxy)
-                    }
-
-                    try {
-                        cameraProviderFuture.get().bindToLifecycle(
+                        camera = cameraProviderFuture.get().bindToLifecycle(
                             lifecycleOwner,
                             selector,
                             preview,
+                            imageCapture,
                             imageAnalysis
                         )
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                    }
-                    previewView
-                },
-                modifier = Modifier.weight(1f)
-            )
+
+                        imageAnalysis.setAnalyzer(
+                            ContextCompat.getMainExecutor(context)
+                        ) { imageProxy ->
+                            processImageProxy(barcodeScanner, imageProxy) { barcodes ->
+                                barcodes.forEach {
+                                    Timber.d(it.rawValue.toString())
+                                    code = it.rawValue.toString()
+                                }
+                            }
+                        }
+
+                        try {
+                            cameraProviderFuture.get().bindToLifecycle(
+                                lifecycleOwner,
+                                selector,
+                                preview,
+                                imageAnalysis
+                            )
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                        previewView
+                    },
+                    modifier = Modifier.fillMaxSize()
+                )
+
+                Surface(
+                    border = BorderStroke(
+                        1.dp,
+                        color = Color.Green
+                    ),
+                    color = Color.Transparent,
+                    modifier = Modifier
+                        .size(
+                            if (LocalConfiguration.current.screenWidthDp < LocalConfiguration.current.screenHeightDp) {
+                                LocalConfiguration.current.screenWidthDp.times(0.6).dp
+                            } else {
+                                LocalConfiguration.current.screenHeightDp.times(0.6).dp
+                            }
+                        )
+                        .align(Alignment.Center)
+                ) {}
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.Bottom
+                ) {
+
+                    Icon(
+                        modifier = Modifier.padding(32.dp),
+                        painter = painterResource(id = R.drawable.ic_gallery),
+                        contentDescription = "Open Gallery",
+                        tint = Color.White
+                    )
+
+                    Icon(
+                        modifier = Modifier
+                            .clickable {
+                                isTorchEnable = !isTorchEnable
+                                if (camera?.cameraInfo?.hasFlashUnit() == true) {
+                                    camera?.cameraControl?.enableTorch(isTorchEnable)
+                                }
+                            }
+                            .padding(32.dp),
+                        painter = painterResource(id = R.drawable.ic_flash),
+                        contentDescription = "Turn On Flash",
+                        tint = Color.White
+                    )
+
+                }
+
+            }
         }
         Text(
             text = code,
@@ -113,6 +202,7 @@ fun QRScannerScreen() {
             fontWeight = FontWeight.Bold,
             modifier = Modifier
                 .fillMaxWidth()
+                .weight(1f)
                 .padding(32.dp)
         )
 
@@ -123,15 +213,14 @@ fun QRScannerScreen() {
 fun processImageProxy(
     barcodeScanner: BarcodeScanner,
     imageProxy: ImageProxy,
+    onSuccess: (barcodes: List<Barcode>) -> Unit,
 ) {
     val inputImage =
         InputImage.fromMediaImage(imageProxy.image!!, imageProxy.imageInfo.rotationDegrees)
 
     barcodeScanner.process(inputImage)
         .addOnSuccessListener { barcodes ->
-            barcodes.forEach {
-                Timber.d(it.rawValue.toString())
-            }
+            onSuccess(barcodes)
         }
         .addOnFailureListener {
             Timber.e(it.message.toString())
